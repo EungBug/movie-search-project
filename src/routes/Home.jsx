6cfe6e5ddbe3622cb2fe9d8f9f30203a11072e4e
EungBug <eungb14@gmail.com>
2023-04-24 20:59:25 +0900

    Add Feature Infinite Scroll

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -10,10 +10,28 @@ export default function Home() {
   const [searchKeyword, setSearchKeyword] = useState('')
   const [movies, setMovies] = useState([])
   const [page, setPage] = useState(1)
-  const [pageMax, setPageMax] = useState(1)
   const [totalResults, setTotalResults] = useState('')
   const [isSearching, setIsSearching] = useState(false)
   const [isLoading, setIsLoading] = useState(false)
+
+  // 무한스크롤
+  const obsRef = useRef(null)
+  const preventRef = useRef(false)
+  const endRef = useRef(false)
+
+  useEffect(() => {
+    //옵저버 생성
+    const observer = new IntersectionObserver(obsHandler, { threshold: 0.5 })
+    if (obsRef.current) {
+      console.log('옵저버 생성')
+      observer.observe(obsRef.current)
+    }
+
+    return () => {
+      console.log('옵저버 disconnect')
+      observer.disconnect()
+    }
+  }, [isSearching])
 
   useEffect(() => {
     // 30개씩 호출 구현
@@ -28,18 +46,25 @@ export default function Home() {
 
     try {
       const res = await searchByMovie(searchKeyword, page)
-      if (res) {
+      if (res.Search) {
         setMovies([...movies, ...res.Search])
         setTotalResults(res.totalResults)
 
         const pageMax = Math.ceil(Number(res.totalResults) / 30)
-        setPageMax(pageMax)
+        console.log('api 호출 =====', page)
+
+        // 더이상 호출할 데이터 없음
+        if (page === pageMax) {
+          endRef.current = true
+        }
 
         //30개씩 조회 완료 시 로딩 종료
         if (page % 3 === 0 || page === pageMax) {
           setIsLoading(false)
+          preventRef.current = true
           return
         }
+
         setPage(page + 1)
       }
     } catch (e) {
@@ -56,12 +81,24 @@ export default function Home() {
     }
   }
 
+  const obsHandler = entries => {
+    const target = entries[0]
+    //옵저버 중복 실행 방지를 위한 Flag들
+    if (!endRef.current && target.isIntersecting && preventRef.current) {
+      console.log('핸들러 실행')
+      preventRef.current = false
+      setPage(prev => prev + 1) //페이지 값 증가
+    }
+  }
+
   const searchButtonClick = () => {
     if (searchKeyword.trim()) {
-      setIsSearching(true)
-      setMovies([])
+      preventRef.current = false
+      endRef.current = false
       setPage(1)
-      getMovies(1)
+      setMovies([])
+      setIsSearching(true)
+      if (page === 1) getMovies()
     }
   }
 
@@ -78,11 +115,17 @@ export default function Home() {
         />
 
         {isSearching && movies ? (
-          <MovieList
-            totalResults={totalResults}
-            movies={movies}
-            isLoading={isLoading}
-          />
+          <>
+            <MovieList
+              totalResults={totalResults}
+              movies={movies}
+              isLoading={isLoading && page <= 3}
+            />
+            {/* 하단을 감지할 옵저버 */}
+            <div
+              className=""
+              ref={obsRef}></div>
+          </>
         ) : (
           ''
         )}
